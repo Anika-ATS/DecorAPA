031824ac0f89d8bfff05a47ccfb703e4a87885de
Anika <tabassumanika210@gmail.com>
2024-06-13 22:45:20 +0600

    route,nav,foot done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Componants/HOME/Banner";
+import Accordian from "../Componants/HOME/Accordian";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Accordian></Accordian>
     </div>
   );
 };
